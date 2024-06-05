de194e7db3e44ce641bb06eb95172b6cf3e0ec63
Linn Olsen <linn.olsen@ict-campus.net>
2024-06-05 10:30:26 +0200

    Get made

---
+++ src/functions/Get.js
@@ -0,0 +1,23 @@
+const { app,input } = require('@azure/functions');
+ 
+const cosmosInput = input.cosmosDB({
+    databaseName: 'DemoDatabase',
+    containerName: 'Items',
+    connection: 'CosmosDB',
+    sqlQuery: "select * from c"
+});
+ 
+app.http('LinnGet', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    extraInputs: [cosmosInput],
+    route: 'items',
+    handler: async (request, context) => {
+        const items = context.extraInputs.get(cosmosInput);
+        return {
+            body: JSON.stringify(items),
+            status: 200
+        };
+    }
+});
+ 
